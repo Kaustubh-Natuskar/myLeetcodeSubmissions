819f9984358dc77758bf590bae305e91eae1d338
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2022-01-04 07:52:01 +0530

    Time: 0 ms (100.00%), Space: 38 MB (6.78%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,7 +1,8 @@
 class Solution {
     public int bitwiseComplement(int N) {
         int X = 1;
-        while (N > X) X = X * 2 + 1;
+        while (N > X) 
+            X = X * 2 + 1;
         return N ^ X;
     }
 }
