ccc382f901cefddd032ba47500438f4daf03e4c8
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2022-01-28 22:17:34 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (8.56%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
@@ -20,12 +20,17 @@ class Solution {
         return toBST(nums,0,nums.length-1);
     }
     public TreeNode toBST(int[] nums, int low, int high){
+        
         if(low>high)
             return null;
+        
         int mid = low+(high-low)/2;
+        
         TreeNode root = new TreeNode(nums[mid]);
+        
         root.left=toBST(nums,low,mid-1);
         root.right=toBST(nums,mid+1,high);
+        
         return root;
     }
 }
