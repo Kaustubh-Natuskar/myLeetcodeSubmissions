15a72fb2a078c9b0af826e9df7fe279b813529fd
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2022-01-29 18:11:57 +0530

    Time: 1 ms (67.89%), Space: 47.3 MB (5.99%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
@@ -23,7 +23,7 @@
  *     }
  * }
  */
-class Solution {
+/*class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         
         ListNode temp = head;
@@ -52,6 +52,32 @@ class Solution {
         
         root.left=toBST(list,low,mid-1);
         root.right=toBST(list,mid+1,high);
+        
+        return root;
+    }
+} */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head){
+        
+        if(head == null)
+            return null;
+        return toBST(head,null);
+    }
+    public TreeNode toBST(ListNode head,ListNode tail){
+        if(head == tail)
+            return null;
+        ListNode fast=head;
+        ListNode slow=head;
+        
+        while(fast!=tail && fast.next!=tail){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        
+        TreeNode root = new TreeNode(slow.val);
+        
+        root.left = toBST(head,slow);
+        root.right = toBST(slow.next,tail);
         
         return root;
     }
