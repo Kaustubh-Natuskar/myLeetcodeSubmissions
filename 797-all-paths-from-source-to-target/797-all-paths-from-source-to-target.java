ce6e38b5eb078d415bc8245b601fecc65355511c
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2022-02-02 17:11:32 +0530

    Time: 3 ms (75.98%), Space: 55.2 MB (5.03%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -3,7 +3,9 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         boolean[] visited = new boolean[graph.length];
         int len = graph.length;
+        
         helper(graph, ans, 0, len, new ArrayList<Integer>(), visited);
+        
         return ans;
     }
      public void helper(int[][] graph, List<List<Integer>> ans, int node, 
@@ -16,12 +18,16 @@ class Solution {
              currPath.remove(currPath.size()-1);
              return;
          }
+         
          if(visited[node] == true)
              return;
+         
          visited[node] = true;
          currPath.add(node);
+         
          for(Integer neighbour : graph[node])
              helper(graph, ans, neighbour, len, currPath, visited);
+         
          visited[node]=false;
          currPath.remove(currPath.size()-1);
      }
