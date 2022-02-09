5ca450b042dbaa762c7329cd56373b80833a6098
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2022-02-09 18:53:45 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -30,32 +30,71 @@ class GFG {
     }
 }// } Driver Code Ends
 
-
+class BFSNode{
+    int node;
+    int parent;
+    BFSNode(int n, int p){
+        this.node=n;
+        this.parent=p;
+    }
+}
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
         // Code here
         boolean[] vis = new boolean[v];
+        
+       /* for(int i=0; i<v; i++){
+            if(vis[i] ==false)
+            if(cycleDetectDFS(i,vis,adj,-1))
+                return true;
+         }
+         */
+        
         for(int i=0; i<v; i++){
             if(vis[i] ==false)
-            if(cycleDetect(i,vis,adj,-1))
+            if(cycleDetectBFS(i, adj, vis))
                 return true;
         }
         return false;
     }
-    public boolean cycleDetect(int node,  boolean[] vis,
+    
+   /* public boolean cycleDetectDFS(int node,  boolean[] vis,
             ArrayList<ArrayList<Integer>> adj, int prev){
                 
                 vis[node] = true;
                 for(Integer i : adj.get(node)){
                     if(vis[i] == false){
-                        if(cycleDetect(i, vis, adj, node) == true) 
+                        if(cycleDetectDFS(i, vis, adj, node) == true) 
                             return true;
                     }else if(i != prev){
                         return true;
                     }
                 }
                 return false;
+            } */
+            
+    public boolean cycleDetectBFS(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis){
+        
+        Queue<BFSNode> q = new LinkedList<>();
+        q.add(new BFSNode(node, -1));
+        vis[node]=true;
+        
+        while(!q.isEmpty()){
+            int n = q.peek().node;
+            int p = q.peek().parent;
+            q.remove();
+            
+            for(Integer item : adj.get(n)){
+                if(vis[item] == false){
+                    q.add(new BFSNode(item, n));
+                    vis[item] = true;
+                }else if(item != p){
+                    return true;
+                }
             }
+        }
+        return false;
+    }        
             
 }
