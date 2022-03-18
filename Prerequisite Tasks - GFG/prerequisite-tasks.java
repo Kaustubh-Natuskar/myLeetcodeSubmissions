4e0a8c39e4d25afe7c84172760d25e71014dc6ab
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2022-03-18 21:09:48 +0530

    Added solution - LeetHub

--- Prerequisite Tasks - GFG/prerequisite-tasks.java
+++ Prerequisite Tasks - GFG/prerequisite-tasks.java
@@ -40,45 +40,10 @@ class GFG {
 //User function Template for Java
 
 class Solution {
-    public boolean isPossible(int N, int[][] prerequisites)
-    {
-        // Your Code goes here
-        // Your Code goes here
-        int n=prerequisites.length;
-        //ArrayList<Integer> list[]= new ArrayList[N];
-         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-         for(int i=0;i<N;i++){
-           list.add(new ArrayList<>());
-       }
-         for(int i=0;i<prerequisites.length;i++){
-           list.get(prerequisites[i][0]).add(prerequisites[i][1]);
-       }
-         
-        boolean vis[]= new boolean[N];
-        boolean dfsvis[]= new boolean[N];
-        for(int i=0;i<N;i++){
-           vis[i]=false;
-       }
-       for(int i=0;i<N;i++){
-           dfsvis[i]=false;
-       }
-        //for (int i=0;i<N;i++){
-            //list[i]= new ArrayList<>();
-       // }
-       // for (int i=0;i<n;i++){
-            //list[prerequisites[i][0]].add(prerequisites[i][1]);
-        //}
-        for (int i=0;i<N;i++){
-            if (vis[i]==false){
-                if (check(i,list,vis,dfsvis))return false;
-            }
-        }
-        return true;
-    }
-    static boolean check(int node,ArrayList<ArrayList<Integer>> list,boolean vis[],boolean dfs[]){
+static boolean check(int node,ArrayList<Integer> list[],boolean vis[],boolean dfs[]){
         vis[node]=true;
         dfs[node]=true;
-        for (int i:list.get(node)){
+        for (int i:list[node]){
             if (vis[i]==false){
                 if (check(i,list,vis,dfs))return true;
             }
@@ -87,6 +52,26 @@ class Solution {
         dfs[node]=false;
         return false;
     }
-    
+
+    public boolean isPossible(int N, int[][] prerequisites)
+    {
+        // Your Code goes here
+        int n=prerequisites.length;
+        ArrayList<Integer> list[]= new ArrayList[N];
+        boolean vis[]= new boolean[N];
+        boolean dfsvis[]= new boolean[N];
+        for (int i=0;i<N;i++){
+            list[i]= new ArrayList<>();
+        }
+        for (int i=0;i<n;i++){
+            list[prerequisites[i][0]].add(prerequisites[i][1]);
+        }
+        for (int i=0;i<N;i++){
+            if (vis[i]==false){
+                if (check(i,list,vis,dfsvis))return false;
+            }
+        }
+        return true;
+    }
 }
 
