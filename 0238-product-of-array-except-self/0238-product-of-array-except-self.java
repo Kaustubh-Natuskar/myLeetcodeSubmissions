5a92497c117411e8a787ed9458b9836de91c65cd
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2024-12-26 18:00:58 +0530

    Time: 3 ms (25.1%), Space: 55.9 MB (31.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -12,7 +12,7 @@ class Solution {
         }
         return res; //TLE //O(N^2)
         */
-        int[] suf = new int[nums.length];
+        /*int[] suf = new int[nums.length];
         int[] pre = new int[nums.length];
         int[] res = new int[nums.length];
         Arrays.fill(suf, 1);
@@ -41,8 +41,38 @@ class Solution {
         }
         return res;
         /**
-        Time: O(N)
+        Time: O(N) + O(N)
         Space: O(N)
-         */
+        */
+
+        int[] suf = new int[nums.length];
+        int[] res = new int[nums.length];
+        Arrays.fill(suf, 1);
+
+        int prod = 1;
+        for(int i=nums.length-1; i>=0; i--){
+            prod = nums[i] * prod;
+            suf[i] = prod;
+        }
+
+        prod = 1;
+        for(int i=0; i<nums.length-1; i++){ //storing prefix product into input array
+            prod = nums[i] * prod;
+            nums[i] = prod;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            if(i == 0){
+                res[i] = suf[i+1];
+                continue;
+            }
+            if(i == nums.length-1){
+                res[i] = nums[i-1];
+                continue;
+            }
+            res[i] = suf[i+1] * nums[i-1];
+        }
+        return res;
+
     }
 }
