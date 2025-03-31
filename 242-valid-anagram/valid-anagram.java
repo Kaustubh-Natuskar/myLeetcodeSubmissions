abfa3dd09408fbbe22b6ce00b40bf63ec52412d3
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2025-03-31 22:58:40 +0530

    Time: 16 ms (23.06%) | Memory: 44.4 MB (58.72%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++)
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+
+        for(int i=0; i<t.length(); i++)
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+
+        return sMap.equals(tMap);
+    }
+}
