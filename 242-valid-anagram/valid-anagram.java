d2c6bd5ec8f5aa31d7ba13b4ede6da82a1e30244
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2025-03-31 23:22:27 +0530

    Time: 18 ms (16.27%) | Memory: 44.9 MB (26.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        /* //Two Maps
         if(s.length() != t.length())
             return false;
 
@@ -12,6 +13,29 @@ class Solution {
         for(int i=0; i<t.length(); i++)
             tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
 
-        return sMap.equals(tMap);
+        return sMap.equals(tMap); 
+        */
+
+        if(s.length() != t.length())
+            return false;
+
+        HashMap<Character, Integer> sMap = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++)
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+
+        for(int i=0; i<t.length(); i++){
+            char tChar = t.charAt(i);
+
+            if(!sMap.containsKey(tChar))
+                return false;
+            
+            sMap.put(tChar, sMap.get(tChar) - 1);
+
+            if(sMap.get(tChar) == 0)
+                sMap.remove(tChar);
+        }
+        
+        return sMap.isEmpty();
     }
 }
