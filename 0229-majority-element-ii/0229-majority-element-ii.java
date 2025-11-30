396ecd6014ae5df7835abfd378aff1a62daedbc2
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2025-11-30 19:07:02 +0530

    Time: 2 ms (99.81%), Space: 52.4 MB (29.95%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,37 +1,41 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE, n = nums.length;
-        int cnt1 = 0;
-        int cnt2 = 0;
-        for(int i=0; i<n; i++){
-            int curr = nums[i];
-            if(cnt1 == 0 && curr != ele2){
-                ele1 = curr;
-                cnt1++;
-            } else if(cnt2 == 0 && curr != ele1){
-                ele2 = curr; 
-                cnt2++;
-            } else if(curr == ele1)
-                cnt1++;
-            else if(curr == ele2)
-                cnt2++;
-            else{
-                cnt1--; 
-                cnt2--;
+    public List<Integer> majorityElement(int[] n) {
+        List<Integer> ans = new ArrayList<Integer>();
+        int m1=0, m2=1;
+        int c1=0, c2=0;
+
+        for(int i=0; i<n.length; i++) {
+            if(m1 == n[i])
+                c1 = c1+1;
+            else if(m2 == n[i])
+                c2 = c2+1;
+            else if(c1 == 0) {
+                m1 = n[i];
+                c1=1;
+            } else if(c2 == 0) {
+                m2 = n[i];
+                c2=1;
+            } else {
+                c1 = c1-1; 
+                c2 = c2-1;
             }
         }
-        cnt1 = 0;
-        cnt2 = 0;
-        int min = (int)(n/3) + 1;
-        for(int i=0; i<n; i++){
-            if(nums[i] == ele1) cnt1++;
-            if(nums[i] == ele2) cnt2++;
+
+        c1=0;
+        c2=0;
+        for(int i=0; i<n.length; i++) {
+            if(n[i] == m1)
+                c1 = c1 +1;
+            else if(n[i] == m2)
+                c2 = c2 +1;
         }
 
-        ArrayList<Integer> l = new ArrayList<Integer>();
-        if(cnt1 >= min) l.add(ele1);
-        if(cnt2 >= min) l.add(ele2);
+        if(c1 > n.length/3)
+            ans.add(m1);
 
-        return l; 
+        if(c2 > n.length/3)
+            ans.add(m2);
+
+        return ans;
     }
 }
