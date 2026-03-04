3e9a208db9b3b763513e81d7177607f113c68de7
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2026-03-04 23:56:38 +0530

    Time: 8 ms (90.91%), Space: 48.8 MB (95.25%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -3,17 +3,20 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         ArrayList<int[]> ans = new ArrayList<int[]>();
         ans.add(intervals[0]);
+
         for(int i=1; i<intervals.length; i++) {
-            int[] last = ans.get(ans.size()-1);
+            int[] lastInterval = ans.get(ans.size() - 1);
             int[] curr = intervals[i];
 
-            if(curr[0] <= last[1]) {
-                last[1] = Math.max(last[1], curr[1]);
+            if(curr[0] <= lastInterval[1]) {
+                lastInterval[1] = Math.max(lastInterval[1], curr[1]);
             } else {
-                ans.add(intervals[i]);
+                ans.add(curr);
             }
+
         }
 
         return ans.toArray(new int[ans.size()][]);
+
     }
 }
